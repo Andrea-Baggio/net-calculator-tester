3923ed6690a54c7a7b68e4f39deead11e1f8cb05
Andrea-Baggio <andreabaggio800@gmail.com>
2023-04-04 14:04:16 +0200

    Add project files.

---
+++ CalculatorTester/UnitTest1.cs
@@ -0,0 +1,20 @@
+using net_calculator_tester;
+
+namespace CalculatorTester
+{
+    public class Tests
+    {
+        Calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestAddition()
+        {
+            
+        }
+    }
+}
---
+++ net-calculator-tester/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_calculator_tester
+{
+    public class Calculator
+    {
+        public float Add(float num1, float num2) => num1 + num2;
+        public float Subtract(float num1, float num2) => num1 - num2;
+        public float Divide(float num1, float num2)
+        {
+            if (num2 is 0) throw new DivideByZeroException();
+            return num1 / num2;
+        }
+        public float Multiply(float num1, float num2) => num1 * num2;
+    }
+}
